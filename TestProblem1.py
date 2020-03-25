ea2daeeb4289887e933fefe615f948001b9babae
Keshav <km02@bu.edu>
2020-03-24 21:47:31 -0400

    fixed assert statement

--- TestProblem1.py
+++ TestProblem1.py
@@ -1,9 +1,9 @@
 import unittest
 
 class TestProblem1(unittest.TestCase):
-    def __init__(): pass
+    def __init__(self): pass
     
-    def testProtocol():
+    def testProtocol(self):
         url = 'https://www.google.com/some-path'
         protocol = getProtocol(url)
-        assertEquals(protocol, 'https')
+        self.assertEqual(protocol, 'https')
