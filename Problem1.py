48cf84933a1009257abf19d802669798cb29e14e
Keshav <km02@bu.edu>
2020-03-24 22:50:50 -0400

    tested path

--- Problem1.py
+++ Problem1.py
@@ -3,10 +3,21 @@ class URL_Splitting():
         self.url = url
 
     def getProtocol(self):
+        """
+        assuming that anything before "://" in a url should be the protocol
+        """
         return self.url.split('://')[0]
 
     def getDomain(self):
+        """
+        assuming that "anything.anything.anything" can be a domain
+        eg: docs.python.org
+        """
         return self.url.split('/')[2]
 
     def getPath(self):
-        return self.url.split('/')[3:]
+        """
+        assuming that the path has no sub-paths
+        i.e. the path has no '/' in it
+        """
+        return self.url.split('/')[3]
--- TestProblem1.py
+++ TestProblem1.py
@@ -13,12 +13,12 @@ class TestProblem1(unittest.TestCase):
         protocol = splitter.getProtocol()
         self.assertEqual(protocol, 'ftp')
 
-        url = 'http://www.facebook.com/home.php'
+        url = 'http://www.facebook.com/home.php/'
         splitter = URL_Splitting(url)
         protocol = splitter.getProtocol()
         self.assertEqual(protocol, 'http')
 
-        url = 'https://docs.python.org/3/library/unittest.html'
+        url = 'https://docs.python.org/3/'
         splitter = URL_Splitting(url)
         protocol = splitter.getProtocol()
         self.assertEqual(protocol, 'https')
@@ -34,12 +34,12 @@ class TestProblem1(unittest.TestCase):
         domain = splitter.getDomain()
         self.assertEqual(domain, 'bu.edu')
 
-        url = 'http://www.facebook.com/home.php'
+        url = 'http://www.facebook.com/home.php/'
         splitter = URL_Splitting(url)
         domain = splitter.getDomain()
         self.assertEqual(domain, 'www.facebook.com')
 
-        url = 'https://docs.python.org/3/library/unittest.html'
+        url = 'https://docs.python.org/3/'
         splitter = URL_Splitting(url)
         domain = splitter.getDomain()
         self.assertEqual(domain, 'docs.python.org')
@@ -49,6 +49,21 @@ class TestProblem1(unittest.TestCase):
         splitter = URL_Splitting(url)
         path = splitter.getPath()
         self.assertEqual(path, 'some-path')
+
+        url = 'ftp://bu.edu/'
+        splitter = URL_Splitting(url)
+        path = splitter.getPath()
+        self.assertEqual(path, '')
+
+        url = 'http://www.facebook.com/home.php/'
+        splitter = URL_Splitting(url)
+        path = splitter.getPath()
+        self.assertEqual(path, 'home.php')
+
+        url = 'https://docs.python.org/3/'
+        splitter = URL_Splitting(url)
+        path = splitter.getPath()
+        self.assertEqual(path, '3')
 
 if __name__ == "__main__":
     unittest.main()
