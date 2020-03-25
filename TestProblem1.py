c64a0cc3be8c6b6ab510510cfbf49f83aff64e5c
Keshav <km02@bu.edu>
2020-03-24 21:52:01 -0400

    removed init

--- TestProblem1.py
+++ TestProblem1.py
@@ -1,8 +1,6 @@
 import unittest
 
 class TestProblem1(unittest.TestCase):
-    def __init__(self): pass
-    
     def testProtocol(self):
         url = 'https://www.google.com/some-path'
         protocol = getProtocol(url)
