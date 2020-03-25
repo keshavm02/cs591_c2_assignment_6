0be1751a2d139becf5332106971ce3e36aa0b056
Keshav <km02@bu.edu>
2020-03-24 22:08:01 -0400

    added more tests for protocol and all passed

--- TestProblem1.py
+++ TestProblem1.py
@@ -8,5 +8,20 @@ class TestProblem1(unittest.TestCase):
         protocol = splitter.getProtocol()
         self.assertEqual(protocol, 'https')
 
+        url = 'ftp://bu.edu/'
+        splitter = URL_Splitting(url)
+        protocol = splitter.getProtocol()
+        self.assertEqual(protocol, 'ftp')
+
+        url = 'http://www.facebook.com/home.php'
+        splitter = URL_Splitting(url)
+        protocol = splitter.getProtocol()
+        self.assertEqual(protocol, 'http')
+
+        url = 'https://docs.python.org/3/library/unittest.html'
+        splitter = URL_Splitting(url)
+        protocol = splitter.getProtocol()
+        self.assertEqual(protocol, 'https')
+
 if __name__ == "__main__":
     unittest.main()
