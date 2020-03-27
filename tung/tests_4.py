947e882e1f9f3f8c297ec6877a017f7bd715c867
Tung <tungbtruong@gmail.com>
2020-03-27 14:59:44 -0600

    Forgot to add the changes from tests_4

--- tung/tests_4.py
+++ tung/tests_4.py
@@ -17,75 +17,47 @@ Explanation: The digit 1 occurs a total of 10 times in: 1, 10, 11, 12, 13, 14, 1
 class test_problem4(unittest.TestCase):
     
     def test_num_occurences(self):
-
-        list = [1,10,11,12,13,14,15,16,17]
         input = 17
         expected = 10
-        actual = prob4.num_occurences(list,input)
+        actual = prob4.num_occurences(input)
         self.assertEqual(expected,actual)
 
-
-        list = [1,10,11,17]
-        input = 17
-        expected = 5
-        actual = prob4.num_occurences(list,input)
+        input = 18
+        expected = 11
+        actual = prob4.num_occurences(input)
         self.assertEqual(expected,actual)
 
-        list = [1,10,11,12,13,14,15,16,17]
         input = 11
         expected = 4
-        actual = prob4.num_occurences(list,input)
+        actual = prob4.num_occurences(input)
+        self.assertEqual(expected,actual)
+
+        input = 0
+        expected = 0
+        actual = prob4.num_occurences(input)
+        self.assertEqual(expected,actual)
+
+        input= -10
+        expected = 0
+        actual = prob4.num_occurences(input)
         self.assertEqual(expected,actual)
 
         
-        list = [10,11,15,1,17,16,18,12,13,14]
-        input = 17
-        expected = 10
-        actual = prob4.num_occurences(list,input)
+        input = 2
+        expected = 1
+        actual = prob4.num_occurences(input)
         self.assertEqual(expected,actual)
 
-        list = [17,17,17,17,17]
-        input = 17
-        expected = 5
-        actual = prob4.num_occurences(list,input)
+        input = 100
+        expected = 21
+        actual = prob4.num_occurences(input)
         self.assertEqual(expected,actual)
 
-        list = [-1,-2,-3]
-        input = 17
-        expected = 0
-        actual = prob4.num_occurences(list,input)
-        self.assertEqual(expected,actual)
 
-        list = [-1,-2,-3, 1]
-        input = -2
-        expected = 0
-        actual = prob4.num_occurences(list,input)
+        input = 1000
+        expected = 301
+        actual = prob4.num_occurences(input)
         self.assertEqual(expected,actual)
-
-        list = [-1,-2,-3, 11]
-        input = 11
-        expected = 2
-        actual = prob4.num_occurences(list,input)
-        self.assertEqual(expected,actual)
-
-        list = [101,111,131,-111]
-        input = 112
-        expected = 5
-        actual = prob4.num_occurences(list,input)
-        self.assertEqual(expected,actual)
-
-        list = [1001]
-        input = 112
-        expected = 0
-        actual = prob4.num_occurences(list,input)
-        self.assertEqual(expected,actual)
-
-        list = [111111]
-        input = 1000000
-        expected = 6
-        actual = prob4.num_occurences(list,input)
-        self.assertEqual(expected,actual)
-        
 
 
 if __name__ == "__main__":
